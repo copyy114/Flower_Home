eecc8f47f53fa4b98dfdcaa842c69f19efe49bfe
Potae-test <kooysky@gmail.com>
2024-08-19 14:33:38 +0700

    edit color bg

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php include('header.html'); ?> 
 
+<!-- <style>
+  .container{
+    background-color: #fff2f3;
+  }
+</style> -->
+
   <main id="main">
 
     <!-- ======= About Section ======= -->
@@ -37,9 +43,6 @@
       </div>
     
     </section><!-- End About Section -->
-    <div class="container">
-    <hr>
-    </div>
 
     <!-- ======= Whu Us Section ======= -->
     <section id="why-us" class="why-us">
@@ -300,7 +303,7 @@
             <div class="swiper-slide">
               <div class="row event-item">
                 <div class="col-lg-6">
-                  <img src="assets/img/h.jpg" class="img-fluid" alt="">
+                  <!-- <img src="assets/img/h.jpg" class="img-fluid" alt=""> -->
                 </div>
                 <div class="col-lg-6 pt-4 pt-lg-0 content">
                   <h3>ช่อดอกไม้ สวยๆ</h3>
@@ -643,57 +646,6 @@
         <iframe style="border:0; width: 100%; height: 350px;" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=7.61557797015850,100.0809003208509+(My%20Business%20Name)&amp;t=k&amp;z=19&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" frameborder="0" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
             <div class="container mt-5">
-<!-- 
-        <div class="info-wrap">
-          <div class="row">
-            <div class="col-lg-3 col-md-6 info">
-              <i class="bi bi-geo-alt"></i>
-              <h4>Location:</h4>
-              <p>A108 Adam Street<br>New York, NY 535022</p>
-            </div>
-
-            <div class="col-lg-3 col-md-6 info mt-4 mt-lg-0">
-              <i class="bi bi-clock"></i>
-              <h4>Open Hours:</h4>
-              <p>Monday-Saturday:<br>11:00 AM - 2300 PM</p>
-            </div>
-
-            <div class="col-lg-3 col-md-6 info mt-4 mt-lg-0">
-              <i class="bi bi-envelope"></i>
-              <h4>Email:</h4>
-              <p>info@example.com<br>contact@example.com</p>
-            </div>
-
-            <div class="col-lg-3 col-md-6 info mt-4 mt-lg-0">
-              <i class="bi bi-phone"></i>
-              <h4>Call:</h4>
-              <p>+1 5589 55488 51<br>+1 5589 22475 14</p>
-            </div>
-          </div>
-        </div> -->
-
-        <!-- <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-          <div class="row">
-            <div class="col-md-6 form-group">
-              <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-            </div>
-            <div class="col-md-6 form-group mt-3 mt-md-0">
-              <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-            </div>
-          </div>
-          <div class="form-group mt-3">
-            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-          </div>
-          <div class="form-group mt-3">
-            <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-          </div>
-          <div class="my-3">
-            <div class="loading">Loading</div>
-            <div class="error-message"></div>
-            <div class="sent-message">Your message has been sent. Thank you!</div>
-          </div>
-          <div class="text-center"><button type="submit">Send Message</button></div>
-        </form> -->
 
       </div>
     </section><!-- End Contact Section -->
