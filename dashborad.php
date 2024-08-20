b016c60c8ef766ffe5fc9074147a19cddbee06d3
Kraiwi345 <80509035+copyy114@users.noreply.github.com>
2024-08-20 13:35:57 +0700

    edit dashbored new

--- dashborad.php
+++ dashborad.php
@@ -24,7 +24,7 @@
                   <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                       รายการสั่งซื้อ</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">ทั้งหมด: <?php echo htmlspecialchars($totalProduct); ?> ชิ้น</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">ทั้งหมด: <?php echo htmlspecialchars($totalQuantity); ?> ชิ้น</div>
                   </div>
                   <div class="col-auto">
                   <i class="fas fa-shopping-cart fa-2x text-gray-300"></i>
@@ -50,6 +50,7 @@
             </div>
           </div>
           <div class="col-xl-3 col-md-6 mb-4">
+          <a href="manage_product.php" class="text-decoration-none">
             <div class="card border-left-info shadow h-100 py-2">
               <div class="card-body">
                 <div class="row no-gutters align-items-center">
@@ -58,7 +59,7 @@
                     </div>
                     <div class="row no-gutters align-items-center">
                       <div class="col-auto">
-                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">ทั้งหมด: <?php echo htmlspecialchars($totalQuantity); ?> รายการ</div>
+                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">ทั้งหมด: <?php echo htmlspecialchars($totalProduct); ?> รายการ</div>
                       </div>
                     </div>
                   </div>
@@ -68,24 +69,29 @@
                 </div>
               </div>
             </div>
+            </a>
           </div>
           <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-              <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                  <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                      จำนวนผู้ใช้งาน</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">ทั้งหมด: <?php echo htmlspecialchars($totalUser); ?> คน</div>
-                  </div>
-                  <div class="col-auto">
-                    <i class="fas fa-user-group fa-2x text-gray-300"></i>
+            <a href="manage_user.php" class="text-decoration-none">
+              <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                        จำนวนผู้ใช้งาน
+                      </div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">
+                        ทั้งหมด: <?php echo htmlspecialchars($totalUser); ?> คน
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-user-group fa-2x text-gray-300"></i>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </a>
           </div>
-
       </div>
   <div class="row mt-4">
     <div class="col-lg-7 mb-lg-0 mb-4">
